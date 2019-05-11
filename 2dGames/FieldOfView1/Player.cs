4db11b3ec5f2e57913963a31cb7958d1fa931a20
BrandyLover <30638614+BrandyLover@users.noreply.github.com>
2019-05-11 00:29:38 -0300

    Some progress made
    Learned about the [Export] tag, wich makes the propertie visible on the Editor, also, learned that the tile set or the tile map need to have a script attached afterwards, the export tileset function doesn't export scripts

--- 2dGames/FieldOfView1/Player.cs
+++ 2dGames/FieldOfView1/Player.cs
@@ -8,6 +8,8 @@ public class Player : KinematicBody2D
     // private string b = "text";
     [Export] public int PlayerSpeed = 200;
     [Export] public Vector2 LinearVelocity;
+
+    Vector2 targetVelocity;
     
     
 
@@ -19,7 +21,20 @@ public class Player : KinematicBody2D
 
     public override void _PhysicsProcess(float delta)
     {
-         Vector2 targetVelocity = new Vector2();
+        
+        GetInput();
+        GetNode<Node2D>("Rotating").LookAt(GetGlobalMousePosition());
+
+        LinearVelocity = targetVelocity.Normalized() * PlayerSpeed;
+
+        MoveAndSlide(LinearVelocity);
+
+    }
+
+    //Getting Input Vector
+    private void GetInput()
+    {
+        targetVelocity = new Vector2();
 
         if(Input.IsActionPressed("move_up"))
             targetVelocity.y -= 1;
@@ -32,12 +47,11 @@ public class Player : KinematicBody2D
         
         if(Input.IsActionPressed("move_left"))
             targetVelocity.x -= 1;
-        
-        LinearVelocity = targetVelocity.Normalized() * PlayerSpeed;
-
-        MoveAndSlide(LinearVelocity,new Vector2(0,0));
-
     }
+
+
+
+
 
     
 }
